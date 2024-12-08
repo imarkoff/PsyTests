b6d2328a73ae1b18be42ffd158dcd2516109d33c
Gipnoboy <dla.infy.infy@gmail.com>
2024-12-08 21:49:28 +0200

    login page

--- frontend/src/app/login/login.tsx
+++ frontend/src/app/login/login.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import "./login/login.scss"
+import "./login.scss"
 
 export default function Home() {
 
@@ -35,7 +35,6 @@ export default function Home() {
           <a href="">Sing up</a>
         </div>
       </form>
-      <div className="backgr"></div>
     </main>
   );
 }
