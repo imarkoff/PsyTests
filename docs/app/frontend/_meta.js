88fdbbcf03124a11127d6f46c170be5c9e1d18de
Vladyslav Hroshev <iv.markov02@gmail.com>
2025-05-27 18:07:32 +0300

    docs/frontend: Added docs about implementing psychological tests, changed port to 4000

---
+++ docs/app/frontend/_meta.js
@@ -0,0 +1,27 @@
+export default {
+    "#": {
+        type: "separator",
+        title: "Frontend",
+    },
+    index: { },
+    "components-conventions": { },
+    "psychological-tests": { },
+    "app-and-features": { },
+    "managing-requests": { },
+    "##": {
+        type: "separator",
+        title: "Useful Links",
+    },
+    react_link: {
+        title: "React Documentation",
+        href: "https://react.dev/learn",
+    },
+    nextjs_link: {
+        title: "Next.js Documentation",
+        href: "https://nextjs.org/docs",
+    },
+    material_ui_link: {
+        title: "Material UI Documentation",
+        href: "https://mui.com/material-ui/getting-started/overview/",
+    },
+}
---
+++ docs/app/frontend/psychological-tests/_meta.js
@@ -0,0 +1,7 @@
+export default {
+    "tests-conventions": { },
+    "implementing-a-test": { },
+    "creating-question-card": {
+        title: "Creating a QuestionCard",
+    }
+}
