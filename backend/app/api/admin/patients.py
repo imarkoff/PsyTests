e6d5f627aab6ab43bc721d3d067a84e55dfec25f
Vladyslav Hroshev <iv.markov02@gmail.com>
2025-09-15 12:17:01 +0300

    refactor: update pagination handling in get_patients endpoint

--- backend/app/api/admin/patients.py
+++ backend/app/api/admin/patients.py
@@ -1,13 +1,16 @@
 from typing import cast
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, Response
 
-from app.schemas.pagination import PaginatedList, PaginationParams
+from app.schemas.pagination import PaginatedList
 from app.schemas.user import UserDto
 from app.services.user_authenticator import Authenticator
 from app.services.user_service import UserService
 from app.dependenies.services.user_service_di import get_user_service
 from app.dependenies.services.authenticator_di import get_authenticator
 from app.schemas.enums.role import Role
+from app.schemas.pagination import NotParsedPaginationParams
+from app.utils.query_pagination_parser import QueryPaginationParser
+from app.exceptions import PaginationError
 
 router = APIRouter(prefix="/patients", tags=["admin_patients"])
 
@@ -17,16 +20,23 @@ router = APIRouter(prefix="/patients", tags=["admin_patients"])
     response_model=PaginatedList[UserDto]
 )
 async def get_patients(
-    pagination_params: PaginationParams = Query(),
+    not_parsed_pagination_params: NotParsedPaginationParams = Query(),
     authenticator: Authenticator = Depends(get_authenticator),
     user_service: UserService = Depends(get_user_service)
 ):
     await authenticator.auth(role=Role.ADMIN)
 
-    patients = await user_service.get_users_by_role(
-        role=Role.PATIENT, pagination_params=pagination_params)
+    try:
+        pagination_params = QueryPaginationParser.parse(
+            not_parsed_pagination_params
+        )
 
-    patients_dtos = cast(PaginatedList[UserDto], patients)
-    patients_dtos.data = [UserDto.create(user) for user in patients.data]
+        patients = await user_service.get_users_by_role(
+            role=Role.PATIENT, pagination_params=pagination_params)
 
-    return patients_dtos
+        patients_dtos = cast(PaginatedList[UserDto], patients)
+        patients_dtos.data = [UserDto.create(user) for user in patients.data]
+
+        return patients_dtos
+    except PaginationError as e:
+        return Response(status_code=400, content=e.message)
