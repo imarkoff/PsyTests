1ca7e5daa1b3f1c559813da5ff15c40afe13e47e
Vladyslav Hroshev <iv.markov02@gmail.com>
2025-09-22 08:50:23 +0300

    backend/refactor: enhance SQLAlchemyPaginationSorter with improved sorting method and tie-breaker logic

--- backend/app/utils/sqlalchemy/sqlalchemy_pagination_sorter.py
+++ backend/app/utils/sqlalchemy/sqlalchemy_pagination_sorter.py
@@ -5,12 +5,25 @@ from app.schemas.enums.pagination import SortingDirection
 
 
 class SQLAlchemyPaginationSorter:
-    @staticmethod
+    """
+    Utility class for sorting SQLAlchemy queries
+    based on pagination parameters.
+    """
+
+    @classmethod
     def sort[T: object](
+        cls,
         model: Any,
         query: Query[T],
         fields: list[SortedField]
     ) -> Query[T]:
+        """
+        Sort a SQLAlchemy query based on the provided fields.
+        :param model: The SQLAlchemy model class.
+        :param query: The SQLAlchemy query to sort.
+        :param fields: A list of SortedField instances defining the sorting.
+        """
+        
         mutated_query = query
         for field in fields:
             mutated_query = mutated_query.order_by(
@@ -19,4 +32,20 @@ class SQLAlchemyPaginationSorter:
                 else getattr(model, field.field).desc()
             )
 
+        mutated_query = cls._add_tie_breaker(
+            model=model,
+            query=mutated_query,
+            fields=fields
+        )
+
         return mutated_query
+
+    @staticmethod
+    def _add_tie_breaker(
+        model: Any,
+        query: Query[Any],
+        fields: list[SortedField]
+    ) -> Query[Any]:
+        if not any(field.field == 'id' for field in fields):
+            query = query.order_by(getattr(model, 'id').asc())
+        return query
