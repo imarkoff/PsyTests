16fc92e8f438e83452449c5c97578fc4ff6679e1
Vladyslav Hroshev <iv.markov02@gmail.com>
2025-10-28 18:57:13 +0200

    backend/feat: add API versioning support

--- backend/src/core/config/app.config.ts
+++ backend/src/core/config/app.config.ts
@@ -3,4 +3,5 @@ import { parseEnvInt } from './env.util';
 export default () => ({
   port: parseEnvInt('PORT'),
   environment: process.env.NODE_ENV || 'development',
+  apiVersion: process.env.API_VERSION || '1',
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { JwtAuthGuard } from './auth/infrastructure/guards/jwt-auth.guard';
 import { RolesGuard } from './auth/infrastructure/guards/roles.guard';
@@ -10,10 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-  const port = configService.get<string>('PORT');
-  if (!port) {
-    throw new Error('Server port is not specified');
-  }
+  const port = configService.get<string>('PORT')!;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -23,6 +20,12 @@ async function bootstrap() {
   );
 
   app.use(cookieParser());
+
+  const apiVersion = configService.get<string>('apiVersion')!;
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: apiVersion,
+  });
 
   const jwtGuard = app.get(JwtAuthGuard);
   const rolesGuard = app.get(RolesGuard);
