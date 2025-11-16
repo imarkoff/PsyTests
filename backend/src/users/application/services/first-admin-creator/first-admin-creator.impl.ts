c9c0fa1ae807d305c1c2481bc2a3d27237a37302
Vladyslav Hroshev <iv.markov02@gmail.com>
2025-11-16 21:41:32 +0200

    backend/refactor: replace ForbiddenException with custom AdminAlreadyExistsException in FirstAdminCreatorImpl

--- backend/src/users/application/services/first-admin-creator/first-admin-creator.impl.ts
+++ backend/src/users/application/services/first-admin-creator/first-admin-creator.impl.ts
@@ -1,11 +1,12 @@
 import { UserCreateDto } from 'src/users/presentation/dtos/user-create.dto';
 import { FirstAdminCreator } from './first-admin-creator.abstract';
-import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetPaginatedUsersByRoleQuery } from '../../queries/get-paginated-users-by-role/get-paginated-users-by-role.query';
 import { UserRole } from '../../../../shared/enums/user-role.enum';
 import { CreateUserCommand } from '../../commands/create-user/create-user.command';
 import { UserDto } from '../../../presentation/dtos/user.dto';
+import { AdminAlreadyExistsException } from '../../../domain/exceptions/admin-already-exists.exception';
 
 @Injectable()
 export class FirstAdminCreatorImpl implements FirstAdminCreator {
@@ -35,7 +36,7 @@ export class FirstAdminCreatorImpl implements FirstAdminCreator {
       this.logger.debug(
         `No space for the second admin. Already created admin ID: ${adminExists.items[0].id}`,
       );
-      throw new ForbiddenException('Admin user already exists.');
+      throw new AdminAlreadyExistsException();
     }
 
     this.logger.debug('No admin users found. Creating the first admin user.');
